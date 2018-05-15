751e1f9048eaf923122f9a41b6f879bf526d500e
retropean <eric.randalls@gmail.com>
2018-05-15 19:28:57 -0400

    bot successfully ported

--- bot.js
+++ bot.js
@@ -24,8 +24,29 @@ if (!process.env.verify_token) {
     process.exit(1);
 }
 
+/* get date */
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd<10) {
+    dd = '0'+dd
+} 
+if(mm<10) {
+    mm = '0'+mm
+} 
+today = mm + '/' + dd + '/' + yyyy;
+
+var knowledge = require("./responses.json");
+var fs = require('fs');
+
 var Botkit = require('botkit');
 var debug = require('debug')('botkit:main');
+
+const rasa = require('./src/middleware-rasa')({
+  rasa_uri: 'http://localhost:5000',
+  rasa_project: 'default'
+})
 
 // Create the Botkit controller, which controls all instances of the bot.
 var controller = Botkit.facebookbot({
@@ -38,6 +59,9 @@ var controller = Botkit.facebookbot({
     debug: true,
     validate_requests: false,
 });
+
+console.log(rasa)
+controller.middleware.receive.use(rasa.receive)
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
@@ -70,10 +94,114 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
 // If a trigger is matched, the conversation will automatically fire!
 // You can tie into the execution of the script using the functions
 // controller.studio.before, controller.studio.after and controller.studio.validate
+
+controller.hears(['greet'], 'direct_message,direct_mention,mention,message_received,facebook_postback', rasa.hears, function (bot, message) {
+  var friendlist = require("./friends.json");
+  //console.log(friendlist)
+  console.log(message);
+  console.log('Printing user name:');
+  console.log(message.user);
+
+  /* search for your friend in the long list of friendz */
+  var friend_content = [];
+  var i;
+  for (i = 0; i < Object.keys(friendlist).length; ++i)
+  {
+    if (Object.keys(friendlist)[i] == message.user)
+    {
+      friend_content = Object.values(friendlist)[i];
+      /*console.log(knowledge.chatbot_brain.responses.getByIndex(i))*/
+      console.log('Found the friend list entry:');
+      console.log(friend_content);
+    }
+    else
+    {
+      console.log('I dont know this friend, adding to friend list');
+      friendlist[message.user] = {};
+      friendlist[message.user].username = 'no name';
+      friendlist[message.user].friendlevel = 0;
+      friendlist[message.user].datemet = today;
+      console.log(friendlist);
+      friend_content = friendlist[message.user];
+    }
+  }
+
+
+  /* loop through the brain to find the right thing to say*/
+  var convo_content = [];
+  var i;
+  var obj;
+  for (i = 0; i < Object.keys(knowledge.chatbot_brain.responses).length; ++i)
+  {
+    if (Object.keys(knowledge.chatbot_brain.responses)[i] == message.intent.name)
+    {
+      convo_content = Object.values(knowledge.chatbot_brain.responses)[i];
+      console.log('Found it! Its:');
+      console.log(convo_content);
+    }
+  }
+  /* say the thing based on your friend level*/
+  console.log(friend_content.friendlevel);
+  if (message.intent.confidence < .75)
+  {
+    bot.reply(message, convo_content[0].misunderstand);
+  }
+  else if (friend_content.friendlevel < 1)
+  {
+    bot.reply(message, convo_content[0].lvl1);
+  }
+  else if (friend_content.friendlevel >= 1 && friend_content.friendlevel < 4)
+  {
+    bot.reply(message, convo_content[0].lvl2);
+  }
+  else if (friend_content.friendlevel >= 4 && friend_content.friendlevel < 8)
+  {
+    bot.reply(message, convo_content[0].lvl3);
+  }
+  else if (friend_content.friendlevel >= 8 && friend_content.friendlevel < 12)
+  {
+    bot.reply(message, convo_content[0].lvl4);
+  }
+  else if (friend_content.friendlevel >= 12 && friend_content.friendlevel < 16)
+  {
+    bot.reply(message, convo_content[0].lvl5);
+  }
+  else if (friend_content.friendlevel >= 16 && friend_content.friendlevel < 20)
+  {
+    bot.reply(message, convo_content[0].lvl6);
+  }
+
+  /* increment friend points*/
+  friendlist[message.user].friendlevel += convo_content[0].friendpoints;
+
+  /* save friend list */  
+  friendlist_export = JSON.stringify(friendlist);
+  fs.writeFile("./friends.json", friendlist_export, function(err)
+  {
+	if (err) throw err;
+	  console.log('Friends Saved!');
+  });
+
+  /*save log*/
+  message_export = JSON.stringify(message);
+  fs.appendFile("./logofinteractions.json", message_export, function(err)
+  {
+	if (err) throw err;
+	  console.log('Interaction Saved!');
+  });
+})   
+
+
+/*
 if (process.env.studio_token) {
     controller.on('message_received,facebook_postback', function(bot, message) {
         if (message.text) {
 		bot.reply(message, 'I am ALIIIIIIIIIVE ');
+	}
+    });
+}
+*/
+
 //            controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
 //                if (!convo) {
 //                    // no trigger was matched
@@ -92,10 +220,10 @@ if (process.env.studio_token) {
 //                    debug('Botkit Studio: ', err);
 //                }
 //            });
-        }
-    });
-} else {
+/*        
+else {
     console.log('~~~~~~~~~~');
     console.log('NOTE: Botkit Studio functionality has not been enabled');
     console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
 }
+*/
